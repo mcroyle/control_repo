b89cb78168009c75d50e1462cef44821d118bcba
mcroyle <matt.croyle@gmail.com>
2022-07-29 10:37:49 -0400

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,20 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/server.jar':
+    ensure => file,
+    source => 'https://piston-data.mojang.com/v1/objects/8399e1211e95faa421c1507b322dbeae86d604df/server.jar'
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+}
